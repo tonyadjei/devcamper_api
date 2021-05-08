0a92cb7a2e8f3f5381c9c82b17565d4fdd46a3e0
Anthony Teye-Adjei <tonyteyeadjei@gmail.com>
2021-05-08 15:12:28 +0200

    setting up mongoDB and creating CRUD for the bootcamps resource

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,34 +1,86 @@
+const Bootcamp = require('../models/Bootcamp');
+
 // @desc    Get all bootcamps
 // @route   GET /api/v1/bootcamps
 // @access  Public
-module.exports.getBootcamps = (req, res, next) => {
-  res.status(200).send('get all');
+module.exports.getBootcamps = async (req, res, next) => {
+  try {
+    const bootcamps = await Bootcamp.find();
+    res.status(200).json({
+      sucess: true,
+      count: bootcamps.length,
+      data: bootcamps,
+    });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
 };
 
 // @desc    Get a single bootcamp
 // @route   GET /api/v1/bootcamps/:id
 // @access  Public
-module.exports.getBootcamp = (req, res, next) => {
-  res.status(200).send('get single');
+module.exports.getBootcamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.findById(req.params.id);
+    if (!bootcamp) {
+      // when fetching a single document by its _id property, if the id is not valid at all, it will of course return an error, however, if the id is a valid object id but it does not exist in the database, we will get a success, but our data will be null. We need to handle this scenario as well by checking whether the document we got back is null or not.
+      // if we have multiple 'res.status()' in our code, we must return the first one,(even though it is in an if block)
+      return res.status(400).json({ success: false });
+    }
+    res.status(200).json({ success: true, data: bootcamp });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
 };
 
 // @desc    Create new a bootcamp
 // @route   POST /api/v1/bootcamps
 // @access  Private
-module.exports.createBootcamp = (req, res, next) => {
-  res.status(200).send('create bootcamp');
+module.exports.createBootcamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.create(req.body);
+    res.status(201).json({
+      success: true,
+      data: bootcamp,
+    });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
 };
 
 // @desc    Update a bootcamp
 // @route   PUT /api/v1/bootcamps/:id
 // @access  Private
-module.exports.updateBootcamp = (req, res, next) => {
-  res.status(200).send('update bootcamp');
+module.exports.updateBootcamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
+      new: true, // telling mongoose that the response we want back from the async method above is the updated document, not the old one
+      runValidators: true, // telling mongoose to run the validations on update
+    });
+
+    if (!bootcamp) {
+      // we are doing this because once again we are dealing with id's
+      return res.status(400).json({ success: false });
+    }
+    res.status(200).json({ success: true, data: bootcamp });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
 };
 
 // @desc    Delete bootcamp
 // @route   Delete /api/v1/bootcamps/:id
 // @access  Private
-module.exports.deleteBootcamp = (req, res, next) => {
-  res.status(200).send('delete a bootcamp');
+module.exports.deleteBootcamp = async (req, res, next) => {
+  try {
+    const response = await Bootcamp.findByIdAndDelete(req.params.id);
+    if (!response) {
+      return res.status(400).json({ success: false });
+    }
+    res
+      .status(200)
+      .json({ success: true, message: 'Data deleted successfully' });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
 };
---
+++ models/Bootcamp.js
@@ -0,0 +1,100 @@
+const mongoose = require('mongoose');
+
+const bootcampSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please add a name'],
+    unique: true,
+    trim: true, //remove whitespaces
+    maxLength: [50, 'Name can not be more than 50 characters'],
+  },
+  slug: String,
+  description: {
+    type: String,
+    required: true,
+    maxLength: [500, 'Description can not be more than 500 characters'],
+  },
+  website: {
+    type: String,
+    match: [
+      /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/,
+      'Please use a valid URL with HTTP or HTTPS',
+    ],
+  },
+  phone: {
+    type: String,
+    maxLength: [20, 'Phone number can not be longer than 20 characters'],
+  },
+  email: {
+    type: String,
+    match: [/\S+@\S+\.\S+/, 'Please use a valid'],
+  },
+  address: {
+    type: String,
+    required: [true, 'Please add address'],
+  },
+  location: {
+    // GeoJSON Point
+    type: {
+      type: String,
+      enum: ['Point'],
+    },
+    coordinates: {
+      type: [Number],
+      index: '2dsphere',
+    },
+    formattedAddress: String,
+    street: String,
+    city: String,
+    state: String,
+    zipCode: String,
+    country: String,
+  },
+  careers: {
+    // Array of strings
+    type: [String],
+    required: true,
+    enum: [
+      'Web Development',
+      'Mobile Development',
+      'UI/UX',
+      'Data Science',
+      'Business',
+      'Other',
+    ],
+  },
+  averageRating: {
+    type: Number,
+    min: [1, 'Rating must be at least 1'],
+    max: [10, 'Rating can not be more than 10'],
+  },
+  averageCost: Number,
+  photo: {
+    type: String,
+    default: 'no-photo.jpg',
+  },
+  housing: {
+    type: Boolean,
+    default: false,
+  },
+  jobAssistance: {
+    type: Boolean,
+    default: false,
+  },
+  jobGuarantee: {
+    type: Boolean,
+    default: false,
+  },
+  acceptGi: {
+    type: Boolean,
+    default: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Bootcamp = mongoose.model('Bootcamp', bootcampSchema);
+
+module.exports = Bootcamp;
