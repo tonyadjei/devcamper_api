322cb884b5fef3b40c98671461e1378431c62153
Anthony Teye-Adjei <tonyteyeadjei@gmail.com>
2021-05-20 16:29:25 +0200

    Used Nodemailer to send email with a reset password route(that includes the token)

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middleware/async');
+const sendEmail = require('../utils/sendEmail');
 const User = require('../models/User');
 
 // @desc    Register user
@@ -83,12 +84,36 @@ exports.forgotPassword = asyncHandler(async (req, res, next) => {
   // we need to save the document again, so that the resetPasswordToken and resetPasswordExpire fields that was set in the method 'getResetPasswordToken()' would be persisted to the database, and wouldn't just be set locally
   await user.save({ validateBeforeSave: false }); // no need to do validation when saving because we are just adding the resetPasswordToken and resetPasswordExpire, we don't want to waste time doing validation which has already been done initially when the user document was being created
 
-  console.log(resetToken);
+  // Create reset url
+  const resetUrl = `${req.protocol}://${req.get(
+    'host'
+  )}/api/v1/resetpassword/${resetToken}`;
 
-  res.status(200).json({
-    success: true,
-    data: user,
-  });
+  const message = `You are receiving this email because you (or someone else) has requested the reset of a password. Please make a PUT request to : \n\n ${resetUrl}`;
+
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: 'Password reset token',
+      message,
+    });
+
+    res
+      .status(200)
+      .json({ success: true, data: `An email has been sent to ${user.email}` });
+  } catch (err) {
+    // now, if something goes wrong with the sending of the email (with nodemailer package), let's clear out the resetPasswordToken and resetPasswordExpire fileds by setting them to undefined
+    console.log(err);
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
+
+    await user.save({ validateBeforeSave: false });
+
+    return next(
+      new ErrorResponse('Email could not be sent, please try again later.'),
+      500
+    );
+  }
 });
 
 // Get token from model, create cookie and send response
